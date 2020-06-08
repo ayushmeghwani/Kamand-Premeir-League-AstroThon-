7be7c8705a72da6324f8db34ec4133716f7adcbd
nidhijainpnp <12njain06@gmail.com>
2020-06-08 18:44:25 +0530

    q2

---
+++ q2/main.py
@@ -0,0 +1,22 @@
+# all imports below
+
+"""
+Any extra lines of code (if required)
+as helper for this function.
+"""
+
+def findDistance(vrec):
+	'''
+	Parameters
+	----------
+	vrec : a `float`
+	
+	Returns
+	-------
+	a `float`
+	'''
+	
+
+	return vrec/71.0
+##vrec=float(input())
+#print(findDistance(vrec))
